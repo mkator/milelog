83a443a975d6085f759fb9b950b6b763b90c8be1
Hoang Tran <strykator@gmail.com>
2023-02-18 22:45:03 -0600

    install icons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React, {type PropsWithChildren} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
 import {fonts} from '../../styles'
 
 const Home: React.FC<PropsWithChildren<{}>> = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
+      <Icon name="home" size={30} color="#900" />
     </View>
   )
 }
